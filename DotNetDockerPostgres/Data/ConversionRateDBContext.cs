58d6e4e2131e936599334de264fb3f7148577cc3
Oluwaseun Akinwale <oluwaseunazure@yahoo.com>
2022-09-20 00:44:07 +0100

    setting up npgsql

---
+++ DotNetDockerPostgres/Data/ConversionRateDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DotNetDockerPostgres.Data
+{
+    public class ConversionRateDBContext : DbContext
+    {
+        private readonly IConfiguration _config;
+
+        public ConversionRateDBContext(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(_config.GetConnectionString("WebApiDatabase"));
+        }
+
+        public DbSet<Unit> Units { get; set; }
+    }
+}
---
+++ DotNetDockerPostgres/Data/TableRates.cs
@@ -0,0 +1,9 @@
+﻿namespace DotNetDockerPostgres.Data
+{
+    public class Unit
+    {
+        public int Id { get; set; }
+        public string MetricUnits { get; set; }
+        public string ConversionValue { get; set; }
+    }
+}
