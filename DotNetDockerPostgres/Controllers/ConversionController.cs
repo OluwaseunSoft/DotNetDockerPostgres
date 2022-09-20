75d0a4dfd2b7f980ff18c4e05a0bd2b03c1604fb
Oluwaseun Akinwale <oluwaseunazure@yahoo.com>
2022-09-20 23:53:51 +0100

    changes

---
+++ DotNetDockerPostgres/Controllers/ConversionController.cs
@@ -0,0 +1,35 @@
+﻿using DotNetDockerPostgres.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetDockerPostgres.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConversionController : ControllerBase
+    {
+        private readonly ConversionRateDBContext _context;
+
+        public ConversionController(ConversionRateDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Unit>> GetPlatforms()
+        {
+            try
+            {
+                System.Console.WriteLine("==> Getting Platforms");
+
+                var platformItem = _context.Units.ToList();
+                return Ok(platformItem);
+            }
+            catch(Exception ex)
+            {
+                System.Console.WriteLine(ex.Message);
+                return BadRequest();
+            }
+        }
+    }
+}
--- DotNetDockerPostgres/Program.cs
+++ DotNetDockerPostgres/Program.cs
@@ -1,7 +1,14 @@
+using DotNetDockerPostgres.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<ConversionRateDBContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("WebApiDatabase"));
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
